0f1af81c8b3518124f60219f59896f78ed9a11af
Haz <hyphens@pm.me>
2020-08-15 21:17:03 +0200

    add circular shift

--- bits.py
+++ bits.py
@@ -24,3 +24,12 @@ def tog(data, position):
     '''toggle the bit at position
     position starts from 0, right to left'''
     return data^ (1<<position)
+
+def circular_right(data: bytes, position):
+    '''how to do a circular shift to the right
+    Python ints are arbitrary percision.
+    so we need to use something more predictable'''
+    assert len(data) == 1, "Too many, or too little characters in input"
+    shift = position % 8
+    return [(data[0] >> shift) | (data[0] << (8 - shift)) % 256]
+
