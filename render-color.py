b31b4cdb7cce56b26d789092444161489cfdde70
Haz <hyphens@pm.me>
2020-10-26 06:08:09 +0200

    better version of the color renderer
    works on arbitrary line positions that contain rgb hex values

--- render-color.py
+++ render-color.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
-def rgb(col):
-    col = col.strip("#\r\n\t \x0b\x0c")
+hexcolor = re.compile(r'#([a-fA-F0-9]{3,6})\b')
+
+def render(col):
+    old = col
     lc  = len(col)
-    if not (lc == 6 or lc == 3):
-        return None
     col = "".join([i*2 for i in col]) if lc == 3 else col
     try:
         col = int(col, 16)
     except ValueError:
-        return None
+        return '#'+ old
     else:
-        return (
+        return '#'+ old +\
+        '\x1b[38;2;%d;%d;%dm🔴\x1b[0m' % (
             (col>>16) & 0xff, 
             (col>>8)  & 0xff, 
-            (col>>0)  & 0xff
-        )
+            (col>>0)  & 0xff)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        colors = sys.stdin.readlines()
+        lines = sys.stdin
     else:
-        colors = sys.argv[1:]
-    for i in colors:
-        i = rgb(i)
-        if not i: 
-            continue
-        sys.stdout.write("#%02X%02X%02X " % i)
-        sys.stdout.write("\x1b[48;2;%d;%d;%dm   \x1b[0m\n" % i)
+        lines = sys.argv[1:]
+    for line in lines:
+        line = re.sub(hexcolor, lambda match: render(match[1]), line)
+        sys.stdout.write(line)
