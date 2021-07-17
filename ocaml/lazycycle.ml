efcb9da6c53d57f3ea8cd11bb4ee2c0c6b25b3ab
Haz <hyphens@pm.me>
2021-07-17 14:49:09 +0200

    make it more presentable

--- ocaml/lazycycle.ml
+++ ocaml/lazycycle.ml
@@ -1,34 +1,37 @@
 type 'a node = [] | (::) of 'a * 'a list
- and 'a list = 'a node lazy_t
+ and 'a list = 'a node Lazy.t
 
-let rec append (lazy l) r =
-  match l with
+let unwrap : 'a list -> 'a node = Lazy.force
+let wrap   : 'a node -> 'a list = Lazy.from_val
+
+let rec append l r =
+  match unwrap l with
     | [] -> r
     | x::xs -> lazy (x::append xs r)
 
-let rec take n (lazy xs) = lazy (
-  match xs with
-    | [] -> []
-    | __ when n = 0 -> []
+let rec take n xs = lazy begin
+  match unwrap xs with
+    | [] -> [] | _ when n = 0 -> []
     | x::xs -> x::take (n-1) xs
-  )
+  end
 
-let rec drop n xs =
-  match xs with
-    | lazy [] as v -> v
-    | xs when n = 0 -> xs
-    | lazy (_::xs) -> drop (n-1) xs
+let rec drop n zs = begin
+  match unwrap zs with
+    | [] -> zs | _ when n = 0 -> zs
+    | _::xs -> drop (n-1) xs
+  end
 
-let rec force (lazy xs) =
-  match xs with
+let rec force xs =
+  match unwrap xs with
     | [] -> List.([])
     | x::xs -> List.(x::force xs)
 
 let cycle xs =
-  let rec x = lazy (Lazy.force_val @@ append xs x) in x
+  let rec x = lazy (unwrap @@ append xs x) in x
 
-let xs = lazy(1::lazy(2::lazy(3::lazy [])))
-  (*structure the type in a way that makes this work more naturally *)
+let (@) h t = wrap (h::t)
+
+let xs = 1 @ 2 @ 3 @ lazy[]
 
 let test1 = force @@ take 10 @@ cycle xs
 let test2 = let ys = cycle xs in
